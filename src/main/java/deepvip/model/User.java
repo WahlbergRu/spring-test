b209424105a1173d8eb23fa4412cf390747c64e5
Nikita Allin <nikita_allin@epam.com>
2017-11-14 17:24:53 +0300

    add handle

--- src/main/java/deepvip/model/User.java
+++ src/main/java/deepvip/model/User.java
@@ -1,6 +1,10 @@
 package deepvip.model;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "USERS")
@@ -12,21 +16,28 @@ public class User {
     private long id;
 
     @Column(unique = true)
+    @Size(min=2, max=30)
     private String login;
 
     @Column
+    @Size(min=2, max=30)
     private String name;
 
     @Column
+    @Size(min=2, max=30)
     private String lastName;
 
     @Column
+    @NotEmpty
+    @Email
     private String email;
 
     @Column
+    @Size(min=5)
     private String password;
 
     @Column
+    @Size(min=5, max=255)
     private String affilation;
 
     public User(){
